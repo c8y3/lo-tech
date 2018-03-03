10c28786f4e4eb4bd3772e6b204706ccf87cdba3
c8y3 <25362953+c8y3@users.noreply.github.com>
2018-03-03 15:25:50 +0100

    [add] plugin buble to replace const and let by var

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,8 @@
+import buble from 'rollup-plugin-buble';
+
 export default {
     input: 'src/Application.js',
+    plugins: [ buble() ],
     output: {
         file: 'results/Application.js',
         format: 'iife',
