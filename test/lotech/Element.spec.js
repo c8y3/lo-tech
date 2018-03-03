61313e85025e681ed9bb90d66f883feb9ee5697a
c8y3 <25362953+c8y3@users.noreply.github.com>
2018-03-03 22:07:16 +0100

    [test] Factored test somewhat

--- test/lotech/Element.spec.js
+++ test/lotech/Element.spec.js
@@ -1,18 +1,22 @@
 import Element from '/lotech/Element';
 
 describe('lotech.Element', function() {
+    let subject;
+    let parentNode;
+    let node;
+
+    beforeEach(function() {
+        node = document.createElement('div');
+        subject = Element(node);
+        parentNode = document.createElement('span');
+    });
+
     describe('draw', function() {
         it('should not fail', function() {
-            const node = document.createElement('div');
-            const subject = Element(node);
-            const parentNode = document.createElement('span');
             subject.draw(parentNode);
         });
 
         it('should append the element node to the parent node', function() {
-            const node = document.createElement('div');
-            const subject = Element(node);
-            const parentNode = document.createElement('span');
             subject.draw(parentNode);
             assert.equal(node, parentNode.firstChild);
         });
