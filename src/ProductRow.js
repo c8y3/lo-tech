71d85311165fd86b511acac59fa63e824b9807ea
c8y3 <25362953+c8y3@users.noreply.github.com>
2018-03-05 22:43:01 +0100

    [add] class to indicate prouct is missing

--- src/ProductRow.js
+++ src/ProductRow.js
@@ -2,5 +2,8 @@ import lotech from '/lotech/index';
 
 export default function(product) {
     const name = lotech.Span(product.name);
+    if (!product.stocked) {
+        name.addStyle('ProductRow', 'isMissing');
+    }
     return lotech.Div([name, lotech.Span(product.price)]);
 };
