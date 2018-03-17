eb7e08a1e4902affe58af84d9a8273ca2655e29d
c8y3 <25362953+c8y3@users.noreply.github.com>
2018-03-17 14:19:02 +0100

    [test] Added a test on the parser

--- test/fragments/Parser.spec.js
+++ test/fragments/Parser.spec.js
@@ -1,11 +1,21 @@
 import Parser from '/fragments/Parser';
 
 describe('fragments.Parser', function() {
+    let subject;
+
+    beforeEach(function() {
+        subject = Parser();
+    });
+
     describe('parse', function() {
         it('should not fail', function() {
-            const subject = Parser();
             const result = subject.parse('<div/>');
             assert.equal(result.tagName, 'div');
+        });
+
+        it('should parse attributes', function() {
+            const result = subject.parse('<div class="root"/>');
+            assert.equal(result.attributes['class'], 'root');
         });
     });
 });
