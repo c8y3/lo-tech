b5313b021d83a59a5f24edf57f13bdb97ca06dc2
c8y3 <25362953+c8y3@users.noreply.github.com>
2018-03-17 10:28:12 +0100

    [clean] Changed Application to test templates

--- src/example/templates/Application.js
+++ src/example/templates/Application.js
@@ -1,19 +1,9 @@
-import FilterableProductTable from '/FilterableProductTable';
-
-const PRODUCTS = [
-    { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
-    { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
-    { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
-    { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
-    { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
-    { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
-];
+import Row from '/Row.html.tpl';
 
 export default function() {
     return {
         start: function() {
-            const mainComponent = FilterableProductTable(PRODUCTS);
-            mainComponent.draw(document.body);
+            console.log(Row());
         }
     };
 };
