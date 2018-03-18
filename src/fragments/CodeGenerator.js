ef47e796f8b035ee3af4680635582919c056cfd8
c8y3 <25362953+c8y3@users.noreply.github.com>
2018-03-18 09:47:42 +0100

    [add] Compilation of children variable

--- src/fragments/CodeGenerator.js
+++ src/fragments/CodeGenerator.js
@@ -1,8 +1,28 @@
+function generateElement(htpl) {
+    const children = generateChildren(htpl.children);
+    return 'lotech.createElement(\'' + htpl.tagName + '\', {}, [' + children + '])';
+}
+
+function generateVariable() {
+    return 'children';
+}
+
+function generate(htpl) {
+    if (htpl.type === 'element') {
+        return generateElement(htpl);
+    }
+    if (htpl.type === 'variable') {
+        return generateVariable(htpl);
+    }
+}
+
+function generateChildren(children) {
+    return children.map(generate);
+}
+
 export default function() {
     return {
-        generate(htpl) {
-            return 'lotech.createElement(\'' + htpl.tagName + '\', {}, [])';
-        }
+        generate
     };
 }
 
--- test/fragments/CodeGenerator.spec.js
+++ test/fragments/CodeGenerator.spec.js
@@ -9,15 +9,19 @@ describe('fragments.CodeGenerator', function() {
 
     describe('generate', function() {
         it('should not fail', function() {
-            const result = subject.generate({tagName: 'div'});
-            assert.equal('lotech.createElement(\'div\', {}, [])', result);
+            const result = subject.generate({type: 'element', tagName: 'div', children: []});
+            assert.equal(result, 'lotech.createElement(\'div\', {}, [])');
         });
 
         it('should return a constructor of the node', function() {
-            const result = subject.generate({tagName: 'p'});
-            assert.equal('lotech.createElement(\'p\', {}, [])', result);
+            const result = subject.generate({type: 'element', tagName: 'p', children: []});
+            assert.equal(result, 'lotech.createElement(\'p\', {}, [])');
         });
 
+        it('should append children', function() {
+            const result = subject.generate({type: 'element', tagName: 'div', children: [{type: 'variable'}]});
+            assert.equal(result, 'lotech.createElement(\'div\', {}, [children])');
+        });
 //{"tagName":"div","attributes":{"class":"root"},"children":[{"type":"variable","name":"children"}]}
 
     });
