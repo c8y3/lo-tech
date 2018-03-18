7284e6c4af115cb5058c9d931c4f292dacdaacbb
c8y3 <25362953+c8y3@users.noreply.github.com>
2018-03-18 12:30:05 +0100

    [add] the content of template parameter children is flattened

--- src/fragments/CodeGenerator.js
+++ src/fragments/CodeGenerator.js
@@ -1,17 +1,12 @@
 function generateElement(htpl) {
     let children = generateChildren(htpl.children);
-// TODO think about this here. Compare with JSX, but it seems to me there is the possibility to retrieve each element of the array, and it is handled in the component. Think about this and see how this is done...
-// or maybe do this only for built components and not base components?
-//    return 'lotech.createElement(\'' + htpl.tagName + '\', {}, [' + children + '])';
-// this is a hack for now...
-    if (children.length === 0) {
-        children = '[]';
-    }
-    return 'lotech.createElement(\'' + htpl.tagName + '\', {}, ' + children + ')';
+    return 'lotech.createElement(\'' + htpl.tagName + '\', {}, [' + children + '])';
 }
 
 function generateVariable() {
-    return 'children';
+    // Template parameter {children} is necessarily an array.
+    // It is flattened to be inserted amoung the list of element children.
+    return '...children';
 }
 
 function generate(htpl) {
