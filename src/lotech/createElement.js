86832bbae0c1b14846413c7447ecedd763386dac
c8y3 <25362953+c8y3@users.noreply.github.com>
2018-03-18 12:30:29 +0100

    [clean] renamed name into type

--- src/lotech/createElement.js
+++ src/lotech/createElement.js
@@ -1,6 +1,6 @@
 import Div from '/lotech/Div';
 
 // TODO try to use default argument (children = []) syntax everywhere => chase all if ( === undefined)
-export default function(name, attributes, children = []) {
+export default function(type, attributes, children = []) {
     return Div(children);
 }
