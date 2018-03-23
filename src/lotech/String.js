1bb57a71e8c1a51a95a9a9d5e151ecfb75928aaa
c8y3 <25362953+c8y3@users.noreply.github.com>
2018-03-23 20:41:57 +0100

    [add] setData on String node

--- src/lotech/String.js
+++ src/lotech/String.js
@@ -2,6 +2,12 @@ import Element from '/lotech/Element';
 
 export default function(data) {
     const node = document.createTextNode(data);
+    const element = Element(node);
 
-    return Element(node);
+    return {
+        ...element,
+        setData(data) {
+            node.data = data;
+        }
+    }
 };
--- test/lotech/String.spec.js
+++ test/lotech/String.spec.js
@@ -1,22 +1,35 @@
 import String from '/lotech/String';
 
 describe('lotech.String', function() {
-    let node;
+    let subject;
 
     beforeEach(function() {
-        const parentNode = document.createElement('div');
-        const subject = String('hello');
-        subject.draw(parentNode);
-        node = parentNode.firstChild;
+        subject = String('hello');
     });
+
+    function draw() {
+        const parentNode = document.createElement('div');
+        subject.draw(parentNode);
+        return parentNode.firstChild;        
+    }
 
     describe('draw', function() {
         it('should draw a text node', function() {
+            const node = draw();
             assert.equal(Node.TEXT_NODE, node.nodeType);
         });
 
         it('should draw a text node', function() {
+            const node = draw();
             assert.equal('hello', node.data);
+        });
+    });
+
+    describe('setData', function() {
+        it('should set the node data', function() {
+            subject.setData('bye');
+            const node = draw();
+            assert.equal('bye', node.data);
         });
     });
 });
