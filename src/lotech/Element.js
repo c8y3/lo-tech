9717bde909b4eedad4c1247e0085fec88b1d1bef
c8y3 <25362953+c8y3@users.noreply.github.com>
2018-03-24 18:25:55 +0100

    [add] addClass

--- src/lotech/Element.js
+++ src/lotech/Element.js
@@ -3,8 +3,12 @@ export default function(node) {
         draw(parentNode) {
             parentNode.appendChild(node);
         },
+// TODO remove => replace by addClass (and also have removeClass)
         addStyle(scope, name) {
             node.classList.add(scope + '__' + name);
+        },
+        addClass(name) {
+            node.classList.add(name);
         }
     };
 };
--- test/lotech/Element.spec.js
+++ test/lotech/Element.spec.js
@@ -2,23 +2,34 @@ import Element from '/lotech/Element';
 
 describe('lotech.Element', function() {
     let subject;
-    let parentNode;
-    let node;
 
     beforeEach(function() {
-        node = document.createElement('span');
+        const node = document.createElement('span');
         subject = Element(node);
-        parentNode = document.createElement('div');
     });
+
+    function draw() {
+        const parentNode = document.createElement('div');
+        subject.draw(parentNode);
+        return parentNode.firstChild;
+    };
 
     describe('draw', function() {
         it('should not fail', function() {
-            subject.draw(parentNode);
+            draw();
         });
 
         it('should append the element node to the parent node', function() {
-            subject.draw(parentNode);
-            assert.equal(node, parentNode.firstChild);
+            const node = draw();
+            assert.equal('SPAN', node.tagName);
+        });
+    });
+
+    describe('addClass', function() {
+        it('should add class to the className of the drawn node', function() {
+            const node = draw();
+            subject.addClass('some__class');
+            assert.equal('some__class', node.className);
         });
     });
 });
